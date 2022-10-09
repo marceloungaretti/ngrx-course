f21b133366c675a670652c834c6a8c783e5d52cf
marceloungaretti <marcelo.ungaretti@gmail.com>
2022-10-09 00:47:36 -0300

    Fim da aula 9

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,7 @@
+import { User } from "./model/user.model";
+import { createAction, props } from "@ngrx/store";
+
+export const login = createAction(
+  "[Login Page] User Login",
+  props<{ user: User }>()
+);
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,18 +1,14 @@
 import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
+  ActionReducerMap
 } from '@ngrx/store';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
+export interface AppState {
 
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const reducers: ActionReducerMap<AppState> = {
 
 };
 
