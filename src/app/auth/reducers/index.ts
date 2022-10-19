a837eb3ba924a1c46d56cfb03ca98599b695ed39
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-10-19 20:15:08 -0300

    fim da aula 16

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -18,5 +18,10 @@ export const authReducer = createReducer(
     return {
       user: action.user
     }
+  }),
+  on(AuthActions.logout, (state, action) => {
+    return {
+      user: undefined
+    }
   })
 );
