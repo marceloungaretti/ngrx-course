3e4654fe5a689f7d874f82e3afe750a88421b13a
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2022-10-25 13:03:26 -0300

    fim da aula 47

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,11 +1,12 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
-
+import { CourseEntityService } from '../services/course-entity.service';
+import { LessonEntityService } from '../services/lesson-entity.service';
 
 @Component({
   selector: 'course',
@@ -15,35 +16,29 @@ import {CoursesHttpService} from '../services/courses-http.service';
 export class CourseComponent implements OnInit {
 
   course$: Observable<Course>;
-
   lessons$: Observable<Lesson[]>;
-
   displayedColumns = ['seqNo', 'description', 'duration'];
-
   nextPage = 0;
 
   constructor(
-    private coursesService: CoursesHttpService,
+    private coursesService: CourseEntityService,
+    private lessonsService: LessonEntityService,
     private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-
     const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
 
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
+    this.course$ = this.coursesService.entities$
+      .pipe(
+        map(courses => courses.find(course => course.url))
+      );
 
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
-
+    this.lessons$ = of([])
   }
-
 
   loadLessonsPage(course: Course) {
 
   }
-
 }
